faac6408a67d5a52771bd79a852ab377c6a26985
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-11-25 19:45:34 +0100

    Add home page template

--- src/main/java/com/github/java/demo/controllers/DieticianRegistrationController.java
+++ src/main/java/com/github/java/demo/controllers/DieticianRegistrationController.java
@@ -12,24 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DieticianRegistrationController {
     private final DieticanRepository dieticianRepository;
 
-@Autowired
-    public DieticianRegistrationController (DieticanRepository dieticianRepository) {
+    @Autowired
+    public DieticianRegistrationController(DieticanRepository dieticianRepository) {
         this.dieticianRepository = dieticianRepository;
     }
 
     @GetMapping("/dietic-register")
-    public String prepareRegistrationPage(){
-    return "WEB-INF/jsp/dietician-registration-page.jsp";
+    public String prepareRegistrationPage() {
+        return "dietican-registration-page";
     }
 
-    @PostMapping("/dietic-register")
-    public String processRegistrationPage(String password, String email, String licenceNumber, String name, String lastName){
-        Dietician dietician= new Dietician();
-        dietician.setEmail(email);
-        dietician.setPassword(password);
-        dietician.setName(name);
-        dietician.setLastName(lastName);
-        dietician.setLicenceNumber(licenceNumber);
-    return "WEB-INF/jsp/home-page.jsp";
-    }
 }
