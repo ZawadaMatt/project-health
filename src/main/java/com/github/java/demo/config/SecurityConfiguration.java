283ddd1b7bbec52ec46fa5d4977c5b4b889830ae
Monika Szczepaniak <monika.szczepaniak1996@gmail.com>
2019-11-26 20:26:40 +0100

    rejestracja skonczona

--- src/main/java/com/github/java/demo/config/SecurityConfiguration.java
+++ src/main/java/com/github/java/demo/config/SecurityConfiguration.java
@@ -22,10 +22,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/register-dietician").anonymous()
                 .antMatchers("/register-patient").anonymous()
+                .antMatchers("/add-dietician").permitAll()
+                .antMatchers("/add-patient").permitAll()
                 .antMatchers("/offer").permitAll();
     }
 
