5f2e0ca7b0ce9724cd9acbe72306078c1a1d7657
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-11-26 19:55:02 +0100

    Add post methods

--- src/main/java/com/github/java/demo/config/SecurityConfiguration.java
+++ src/main/java/com/github/java/demo/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.github.java.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -21,13 +24,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/user-register").anonymous()
-                .antMatchers("/user-login").anonymous()
+                .antMatchers("/register-dietician").anonymous()
+                .antMatchers("/register-patient").anonymous()
                 .antMatchers("/offer").permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/com/github/java/demo/controllers/UserRegistrationController.java
+++ src/main/java/com/github/java/demo/controllers/UserRegistrationController.java
@@ -1,22 +1,28 @@
 package com.github.java.demo.controllers;
 
+import com.github.java.demo.domain.Dietician;
 import com.github.java.demo.domain.Patient;
 import com.github.java.demo.repositories.DieticanRepository;
 import com.github.java.demo.repositories.PatientsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class UserRegistrationController {
     private final PatientsRepository patientsRepository;
     private final DieticanRepository dieticanRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserRegistrationController(PatientsRepository patientsRepository, DieticanRepository dieticanRepository) {
+    public UserRegistrationController(PatientsRepository patientsRepository, DieticanRepository dieticanRepository, PasswordEncoder passwordEncoder) {
         this.patientsRepository = patientsRepository;
         this.dieticanRepository = dieticanRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register-dietician")
@@ -30,17 +36,29 @@ public class UserRegistrationController {
     }
 
     @PostMapping("/add-patient")
-    public String addUsertoDataBase(String email, String password) {
+    public String addUsertoDataBase(String email, String name, String lastName, String password, String birthDate, String gender, String phoneNumber) {
         Patient newPatient = new Patient();
         newPatient.setEmail(email);
-        newPatient.setPassword(password);
+        newPatient.setName(name);
+        newPatient.setLastName(lastName);
+        newPatient.setPassword(passwordEncoder.encode(password));
+        newPatient.setBirthDate(LocalDate.parse(birthDate));
+        newPatient.setGender(gender);
+        newPatient.setPhoneNumber(phoneNumber);
+        patientsRepository.save(newPatient);
         return "index";
     }
 
     @PostMapping("/add-dietician")
-    public String addDieticianToDataBase() {
-
-        return "";
+    public String addDieticianToDataBase(String email, String name, String lastName, String password, String licenceNumber) {
+        Dietician newDietician = new Dietician();
+        newDietician.setEmail(email);
+        newDietician.setName(name);
+        newDietician.setLastName(lastName);
+        newDietician.setPassword(passwordEncoder.encode(password));
+        newDietician.setLicenceNumber(licenceNumber);
+        dieticanRepository.save(newDietician);
+        return "index";
     }
 
 }
