b043c636f5d34fa0d439e23e1af65bc118c2b4b7
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-12-03 19:55:14 +0100

    Add ProgressPageController

--- src/main/java/com/github/java/demo/controllers/ProgressPageController.java
+++ src/main/java/com/github/java/demo/controllers/ProgressPageController.java
@@ -1,7 +1,10 @@
 package com.github.java.demo.controllers;
 
+import com.github.java.demo.domain.Dietician;
+import com.github.java.demo.repositories.DieticanRepository;
 import com.github.java.demo.repositories.PatientsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +14,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ProgressPageController {
 
     private PatientsRepository patientsRepository;
+    private DieticanRepository dieticanRepository;
 
     @Autowired
-    public ProgressPageController(PatientsRepository patientsRepository) {
+    public ProgressPageController(PatientsRepository patientsRepository, DieticanRepository dieticanRepository) {
         this.patientsRepository = patientsRepository;
+        this.dieticanRepository = dieticanRepository;
     }
 
     @GetMapping("/{id}")
     public String progressPage(@PathVariable("id") String id, Model model) {
         model.addAttribute("patientProgress", patientsRepository.findPatientById(Long.parseLong(id)));
+        Dietician dietician = dieticanRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        model.addAttribute("dietican", dietician);
         return "panel";
     }
 
