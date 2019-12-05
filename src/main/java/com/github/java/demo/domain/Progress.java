be3e5a2ad61bd86525b394c504425921ddf857bd
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-12-05 20:43:34 +0100

    ingredient-register.html

--- src/main/java/com/github/java/demo/domain/Progress.java
+++ src/main/java/com/github/java/demo/domain/Progress.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class Progress {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private LocalDate date;
