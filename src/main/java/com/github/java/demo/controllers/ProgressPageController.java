0bfe15e8cc94f7b36ae7ee0d55b771ffb579e5ff
Monika Szczepaniak <monika.szczepaniak1996@gmail.com>
2019-12-09 18:40:31 +0100

    panele - kontynuacja

--- src/main/java/com/github/java/demo/controllers/ProgressPageController.java
+++ src/main/java/com/github/java/demo/controllers/ProgressPageController.java
@@ -22,7 +22,7 @@ public class ProgressPageController {
         this.dieticanRepository = dieticanRepository;
     }
 
-    @GetMapping("/progress/{id}")
+    @GetMapping("/{id}")
     public String progressPage(@PathVariable("id") String id, Model model) {
         model.addAttribute("patientProgress", patientsRepository.findPatientById(Long.parseLong(id)));
         Dietician dietician = dieticanRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
