8fd432fbb4c647827e6e84a312fc20169525d157
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-12-09 19:58:52 +0100

    add ProgressRegistrationController

--- src/main/java/com/github/java/demo/repositories/ProgressRepository.java
+++ src/main/java/com/github/java/demo/repositories/ProgressRepository.java
@@ -1,9 +1,14 @@
 package com.github.java.demo.repositories;
 
+import com.github.java.demo.domain.Patient;
 import com.github.java.demo.domain.Progress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProgressRepository extends JpaRepository <Progress, Long> {
+
+    List<Progress> findProgressesByPatient(Patient patient);
 }
